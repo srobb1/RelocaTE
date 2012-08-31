b9879b00c0a842943f75609246bb7439a688e350
srobb1 <sofia2341@gmail.com>
2012-08-30 21:23:36 -0700

    added excision with and without footprint

--- scripts/characterizer.pl
+++ scripts/characterizer.pl
@@ -13,7 +13,7 @@ use Getopt::Long;
 ## for i in `seq 1 12`;do mping_homozygous_heterozygous.pl Chr$i.mping.te_insertion.all.txt ../bam_files/A119_500bp_Chr$i.sorted.bam > A119.Chr$i.inserts_characterized.txt ; done
 ## or
 ## mping_homozygous_heterozygous.pl A119.mping.te_insertion.all.txt ../bam_files/ > A119.inserts_characterized.txt
-##
+## 080112 added the abiltiy to call somatic excision events with no footprint and the option to look for insertions with footprints 
 ## 061312 changed the values for calling homo, het, new
 ## 052912 added getOpts long
 ## 031412 added a check for XM and NM tags in sam line, if >0 then it is not 100% Match
@@ -31,11 +31,13 @@ my $sites_file ;
 my $bam_dir;
 my $genome_fasta;
 my @bam_files;    ## can be a single .bam file or a direcotory containing .bam files
+my $excision = 0;
 
 GetOptions(
   's|sites_file:s'	=> \$sites_file,
   'b|bam_dir:s'         => \$bam_dir,
   'g|genome_fasta:s'    => \$genome_fasta,
+  'x|excision:i'        => \$excision,
 );
 
 sub getHelp {
@@ -47,6 +49,7 @@ options:
 -s file		relocaTE output file: *.te_insertion.all.txt [no default]
 -b dir/file	bam file of the orginal reads aligned to reference (before TE was trimmed) or directory of bam files [no default]
 -g file		reference genome fasta file [no default]
+-x int		find excision events that leave a footprint, yes or no(1|0) [0]
 -h 		this help message
 
 For more information see documentation: http://docs........
@@ -71,7 +74,8 @@ open OUTGFF, ">$cwd/$filename.homo_het.gff";
 print
   "strain\tTE\tTSD\tchromosome.pos\tavg_flankers\tspanners\tstatus\n";
 my %matches;
-
+my %TSDs;
+my %toPrint;
 while ( my $line = <INSITES> ) {
   next if $line =~ /=/;
   next if $line =~ /^\s/;
@@ -79,6 +83,7 @@ while ( my $line = <INSITES> ) {
 
   # mping   A119    Chr1    1448    C:1     R:0     L:1
   my ($te,$TSD,$exp,$chromosome, $pos, $total_string, $right_string, $left_string) = split /\t/, $line;
+  $TSDs{$chromosome}{$pos}=$TSD;
   my ($total_count) = $total_string =~ /C:(\d+)/;
   my ($left_count)  = $left_string  =~ /L:(\d+)/;
   my ($right_count) = $right_string =~ /R:(\d+)/;
@@ -127,15 +132,10 @@ while ( my $line = <INSITES> ) {
           $Mmatch++;
         }elsif (!defined $NM and !defined $XM){
           $Mmatch++;
-        }else{
-          $matches{"$chromosome.$pos"}{sam}{$sam_line} = 1;
         }
       }
       elsif ( $cigar =~ /[IND]/ ) {
         $matches{"$chromosome.$pos"}{sam}{$sam_line} = 1;
-      }else {
-        $matches{"$chromosome.$pos"}{sam}{$sam_line} = 1;
-
       }
     }
     my $spanners         = $Mmatch;
@@ -146,10 +146,10 @@ while ( my $line = <INSITES> ) {
       $status = 'homozygous';
     }
     elsif ( $average_flankers >= 5 and $spanners < 5 ) {
-      $status = 'homozygous?';
+      $status = 'homozygous/excision_no_footprint';
     }
     elsif ($spanners < ($average_flankers * .2) and $spanners <= 10){
-      $status = 'homozygous?';
+      $status = 'homozygous/excision_no_footprint';
     }
     elsif ($average_flankers <= 2 and $spanners > 10){
       $status = 'new_insertion';
@@ -173,64 +173,112 @@ while ( my $line = <INSITES> ) {
     }
 
 
-=cut #original criteria
-   if ( $spanners == 0 ) {
-      $status = 'homozygous';
-    }
-    elsif ( ($spanners - $average_flankers) > ($spanners + $average_flankers)/2){
-      $status = 'new_insertion';
-    }
-    elsif ( abs( $average_flankers - $spanners ) <= 5 ) {
-      $status = 'heterozygous';
-    }
-    elsif ( $average_flankers >= 9 and $spanners < 5 ) {
-      $status = 'homozygous?';
-    }
-    elsif ( $average_flankers >= 5 and $spanners < 5 ) {
-      $status = 'homozygous?';
-    }
-    elsif (
-      abs( $average_flankers - $spanners ) <=
-      ( ( $average_flankers + $spanners ) / 2 ) )
-    {
-      $status = 'heterozygous?';
-    }
-    elsif ($spanners < ($average_flankers * .2) and $spanners <= 10){
-      $status = 'homozygous?';
-    }
-=cut
     $matches{"$chromosome.$pos"}{status} = $status;
-    print "$exp\t$te\t$TSD\t$chromosome.$pos\t$average_flankers\t$spanners\t$status\n"
-      ;    #\t$Smatch\t$cigar_all\n";
-    print OUTGFF
-"$chromosome\t$exp\ttransposable_element_attribute\t$pos\t$pos\t.\t.\t.\tID=$chromosome.$pos.spanners;avg_flankers=$average_flankers;spanners=$spanners;type=$status;TE=$te;TSD=$TSD\n";
-
-#print "$chromosome.$pos\t$total_count\t$left_count\t$right_count\t$Mmatch\t$status\n";#\t$Smatch\t$cigar_all\n";
+    $toPrint{$chromosome}{$pos}{$TSD}{TE} = $te;
+    $toPrint{$chromosome}{$pos}{$TSD}{flank} = $average_flankers;
+    $toPrint{$chromosome}{$pos}{$TSD}{span} = $spanners;
+    $toPrint{$chromosome}{$pos}{$TSD}{status} = $status;
+    $toPrint{$chromosome}{$pos}{$TSD}{strain} = $exp;
   }
 }
+
+if ($excision) {
+
 ##generate vcf of spanners looking for excision events
-__END__
-my @unlink_files;
-foreach my $pos ( keys %matches ) {
-  my ( $target, $loc ) = split /\./, $pos;
-  my $range      = "$target:$pos";
-  my $sam        = "$cwd/$pos.sam";
-  my $bam        = "$cwd/$pos.bam";
-  my $sorted_bam = "$cwd/$pos.sorted";
-  my @sam_lines  = keys %{ $matches{$pos}{sam} };
-  if ( @sam_lines > 1 ) {
-    my $pos_sam = join "\n", @sam_lines;
-    open POSSAM, ">$sam";
-    print POSSAM $pos_sam;
-    `samtools view -bT $genome_fasta $sam > $bam`;
-    `samtools sort $bam $sorted_bam`;
-    `samtools index $sorted_bam.bam`;
+  my @unlink_files;
+  my @vcfs;
+  foreach my $pos ( keys %matches ) {
+    my ( $target, $loc ) = split /\./, $pos;
+    next unless exists $toPrint{$target}{$loc};
+    my $range      = "$target:$pos";
+    my $sam        = "$cwd/$pos.sam";
+    my $bam        = "$cwd/$pos.bam";
+    my $sorted_bam = "$cwd/$pos.sorted";
+    my @sam_lines  = keys %{ $matches{$pos}{sam} };
+    if ( @sam_lines > 1 ) {
+      my $pos_sam = join "\n", @sam_lines;
+      open POSSAM, ">$sam";
+      print POSSAM $pos_sam;
+      `samtools view -bT $genome_fasta $sam > $bam`;
+      `samtools sort $bam $sorted_bam`;
+      `samtools index $sorted_bam.bam`;
 
 `samtools mpileup -C50 -ugf $genome_fasta -r $range  $sorted_bam.bam | bcftools view -bvcg - > $cwd/$pos.var.raw.bcf`;
 `bcftools view $cwd/$pos.var.raw.bcf | vcfutils.pl varFilter -D 100 > $cwd/$pos.var.flt.vcf`;
+      push @vcfs, "$cwd/$pos.var.flt.vcf";
+      push @unlink_files, $sam, $bam, "$sorted_bam.bam.bai", "$sorted_bam.bam",
+        "$cwd/$pos.var.raw.bcf", "$cwd/$pos.var.flt.vcf";
+      close POSSAM;
+    }
+  }
+##Chr1	16327633	.	GAGTACTACAATTAGTA	GAGTA	1930.0%	.	INDEL;DP=2;AF1=1;CI95=0.5,1;DP4=0,0,1,1;MQ=29;FQ=-40.5	GT:PL:GQ	1/1:58,6,0:10
+  open EXCISION, ">>$cwd/excisions_with_footprint.vcfinfo" or die "can't open $cwd/excisions_with_footprint.vcfinfo for writing $!";
+  foreach my $vcf (@vcfs) {
+    ##Chr2.30902247.var.flt.vcf 
+    my @path = split /\// , $vcf;
+    my $file = pop @path;
+    my ( $insert_ref, $insert_pos ) = $file =~ /(.+)\.(\d+)\.var\.flt\.vcf/;
+    my $TSD = $TSDs{$insert_ref}{$insert_pos} ;
+    my $TSD_len   = length $TSD;
+    open VCF, $vcf;
+    while ( my $line = <VCF> ) {
+        next unless $line !~ /^#/;
+        chomp $line;
+        my ( $ref, $first_base, $col_3, $ref_seq, $strain_seq )
+          = split /\t/, $line;
+        my $aln_start = $first_base - $insert_pos - 1;
+        my $aln_end_ref   = $first_base - length($ref_seq) - $insert_pos - 1;
+        my $aln_end_strain =
+          $first_base - length($strain_seq) - $insert_pos - 1;
+        my $aln_end_ref_near_insert_pos    = 0;
+        my $aln_end_strain_near_insert_pos = 0;
+        my $aln_start_near_insert_pos      = 0;
 
-    push @unlink_files, $sam, $bam, "$sorted_bam.bam.bai","$sorted_bam.bam", "$cwd/$pos.var.raw.bcf";
-    close POSSAM;
+        if (  ( $aln_start <= $TSD_len + 1 )
+          and ( ( $aln_start * -1 <= $TSD_len + 1 ) ) )
+        {
+          $aln_start_near_insert_pos = 1;
+        }
+        if (  ( $aln_end_ref <= $TSD_len + 1 )
+          and ( ( $aln_end_ref * -1 <= $TSD_len + 1 ) ) )
+        {
+          $aln_end_ref_near_insert_pos = 1;
+        }
+        if (  ( $aln_end_strain <= $TSD_len + 1 )
+          and ( ( $aln_end_strain * -1 <= $TSD_len + 1 ) ) )
+        {
+          $aln_end_strain_near_insert_pos = 1;
+        }
+        ## if the alignment end in ref or in strain is close to the insertion postion,
+        ## or if one is before and one is after the insertion postion,
+        ## it is a potential excision with footprint
+        if (
+          ( $aln_end_ref_near_insert_pos or $aln_end_strain_near_insert_pos )
+          or ( $aln_end_ref_near_insert_pos * $aln_end_strain_near_insert_pos <=
+            0 )
+          )
+        {
+          print EXCISION "$insert_ref.$insert_pos\t$line\n";
+          my $status = $toPrint{$insert_ref}{$insert_pos}{$TSD}{status};
+          $toPrint{$insert_ref}{$insert_pos}{$TSD}{status} =
+            $status . "/excision_with_footprint";
+        }
+    }
+  }
+  unlink @unlink_files;
+}
+
+foreach my $chr( sort keys %toPrint){
+  foreach my $pos (sort {$a <=> $b} keys %{$toPrint{$chr}}){
+    foreach my $tsd (sort keys %{$toPrint{$chr}{$pos}}){
+      my $TE = $toPrint{$chr}{$pos}{$tsd}{TE};
+      my $flankers = $toPrint{$chr}{$pos}{$tsd}{flank};
+      my $spanners = $toPrint{$chr}{$pos}{$tsd}{span};
+      my $status = $toPrint{$chr}{$pos}{$tsd}{status};
+      my $strain = $toPrint{$chr}{$pos}{$tsd}{strain};
+      print "$strain\t$TE\t$tsd\t$chr.$pos\t$flankers\t$spanners\t$status\n";
+      print OUTGFF
+"$chr\t$strain\ttransposable_element_attribute\t$pos\t$pos\t.\t.\t.\tID=$chr.$pos.spanners;avg_flankers=$flankers;spanners=$spanners;type=$status;TE=$TE;TSD=$tsd\n";
+    }
   }
 }
-unlink @unlink_files;
