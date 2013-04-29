f0f8378528bce48614aa5619512527348c99bf2d
srobb1 <sofia2341@gmail.com>
2013-04-29 13:53:48 -0700

    now will compare RelocaTE output, not characaTErizer output

--- scripts/compare_strains.pl
+++ scripts/compare_strains.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl -w
 use strict;
-#use inserts.characterixed.txt
+#use *confident.nonref.txt 
+
+#TE	TSD	Exper	chromosome	insertion_site	strand	left_flanking_read_count	right_flanking_read_count	left_flanking_seq	right_flanking_seq
+#1360	CACATAG	ab_neurons	2R:1372610..1372616	+	2	25	AAGCGACATATTTAAGAGCACTCATCAACAGAATAGTGCTCTTAAGATGCTATAACGTATTTAGTATTCTAAATCACAATCAAAAGTGGTAACATGCACA	TAGATTAAATACAAACCGAATAGATCCCAGCTTGCGTCATGAACACAGGGATAAGTACTGTAAAGCATTCAAATTATATTTATTGGCTAGGCATCATATG
+#1360	GTACTAC	ab_neurons	2R:5086262..5086268	+	30	43	
 
 #strain  TE      TSD   chromosome.pos    strand  avg_flankers    spanners        status
 #EG4_2   mping   TTA   Chr1:2925..2927   +  32   0       homozygous
@@ -12,9 +16,9 @@ foreach my $file (@insert_files){
   while (my $line = <IN>){
    chomp $line;
    next if $line =~ /chromosome.pos.+avg_flan/;
-      my ($strain, $te, $tsd, $pos, $strand, $spanners, $flankers, $class) = split /\t/ , $line;
-      $class =~ s/\?//;
-      $inserts{$pos}{$strain}=$class;
+      my ($te, $tsd, $strain, $pos, $strand, $left_flankers, $right_flankers) = split /\t/ , $line;
+      #$class =~ s/\?//;
+      $inserts{$pos}{$strain}=$te;
   }
 }
 my %uniq;
@@ -23,30 +27,30 @@ foreach my $pos (keys %inserts){
   my $strains = join ( ',' , (sort @strains));
   ## keep count of how many positions this combination of strains have in common 
   $uniq{$strains}{count}++;
-  my @classes;
+  my @TEs;
   foreach my $strain (sort @strains){
     ## sort classes in same order as strains
-    push @classes , $inserts{$pos}{$strain};
+    push @TEs , $inserts{$pos}{$strain};
   }
-  my $classes = join ( ',' , (@classes));
+  my $TEs = join ( ',' , (@TEs));
   ## keep track of the classifications for this combination of strains at this position
-  $uniq{$strains}{pos}{$pos}=$classes;
+  $uniq{$strains}{pos}{$pos}=$TEs;
 }
-my %class_count;
-print "----each position and the stains that share it and the classifications\n";
+my %TE_count;
+print "----each position and the stains that share it and the TE\n";
 foreach my $strains (keys %uniq){
   foreach my $pos ( keys %{$uniq{$strains}{pos}}){
     #my $class_count;
-    my $classes  = $uniq{$strains}{pos}{$pos};
-    $class_count{$strains}{$classes}++;;
-    print "$strains\t$pos\t$classes\n";
+    my $TEs  = $uniq{$strains}{pos}{$pos};
+    $TE_count{$strains}{$TEs}++;;
+    print "$strains\t$pos\t$TEs\n";
   }
 }
-print "\n\n-----count of how many times this combination of strains share this same set of classifications\n";
-foreach my $strains (keys %class_count){
-  foreach my $classes (keys %{$class_count{$strains}}){
-    my $count = $class_count{$strains}{$classes};
-    print "$strains\t$classes\t$count\n";
+print "\n\n-----count of how many times this combination of strains share this same set of TEs\n";
+foreach my $strains (keys %TE_count){
+  foreach my $TEs (keys %{$TE_count{$strains}}){
+    my $count = $TE_count{$strains}{$TEs};
+    print "$strains\t$TEs\t$count\n";
   }
 }
 print "\n\n-----count of the times this combination of strains share a position\n";
